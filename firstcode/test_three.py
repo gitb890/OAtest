5e9ab094dab4cba2c48bc75586607e7e5f305fcb
gitb890 <2832816260@qq.com>
2022-06-06 23:37:22 +0800

    提交three文件，练习pytest.fixture调用方式

---
+++ firstcode/test_three.py
@@ -0,0 +1,30 @@
+import pytest
+
+# 调用方式1
+@pytest.fixture
+def login():
+    print("输入账号，密码先登录")
+
+def test_s1(login):
+    print("用例1：登录之后其他动作 111")
+
+def test_s2():
+    print("用例2，不用登录 2222")
+
+# 调用方式2
+@pytest.fixture
+def login1():
+    print("please input password")
+
+@pytest.mark.usefixtures("login1","login")
+def test_s11():
+    print("用例2，不需要登录   333")
+
+# 调用方式3
+@pytest.fixture(autouse=True)
+def login2():
+    print("========login==============")
+
+@pytest.mark.usefixtures("login2")
+def loginss():
+    print(1)
