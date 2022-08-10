aad93dc232147f3f0636b5cb3a415c8602a054a3
gitb890 <2832816260@qq.com>
2022-08-10 22:56:26 +0800

    补充用例

--- STD_AUTOTEST/selenium_demo.py
+++ STD_AUTOTEST/selenium_demo.py
@@ -1,6 +1,7 @@
 '''
 selenium的第一个测试用例
 '''
+from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -13,12 +14,18 @@ url = webdriver.Chrome()
 url.get("https://www.baidu.com/")
 
 #找到指定的元素id或name
-el = url.find_element(By.NAME,'wd').send_keys('F12开启开发者工具')
+el = url.find_element(By.NAME,'wd').send_keys('selenium')
 
 #对元素进行文本的操作
-# el.send_keys('F12开启开发者工具')
+# el.send_keys('selenium')
 
 # print(el.get_attribute('value'))
 
 # 执行一次文本框输入查询
 el = url.find_element(By.ID,'su').click()
+
+# 等待
+sleep(5)
+
+# 退出浏览器
+url.quit()
